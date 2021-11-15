f16d5a72ad6687d04dcd3a318c26f1946d24239d
Ahmet Uysaler <ahmetsenih@gmail.com>
2021-11-14 20:06:02 -0600

    fixed functions and updated header

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const pixels = document.getElementsByTagName("td");
 const colors = document.getElementsByTagName("p")
-let pixel = document.getElementsByTagName("td");
+let pixel = document.getElementsByTagName("td")[0];
+let color = ""
 function pixelWasClicked() {
     for (const element of pixels) {
         element.addEventListener("click", function(){
             pixel = element;
+            colorChange();
         })
     }
 }
 function colorChange() {
     for (const element of colors) {
         element.addEventListener("click", function() {
-            pixel.style.backGroundColor = element.id;
-        })
+            color = element.id;
+        }) 
     }
+     pixel.style.backgroundColor = color;
 }
 pixelWasClicked();
 colorChange();
